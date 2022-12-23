727157e1c1df587081a556ea3849680a4b281a38
youngseoji <estell.ys.j@gmail.com>
2022-12-23 17:49:48 +0900

    Upadte : comment

--- window/window.js
+++ window/window.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// 윈도우 객체 : 모든 브라우저의 변수, 자바스크립트, bom, dom을 모두 포함하고 있는 객체
+// 그래서 자바스크립트로 브라우저를 동적으로 변경할 수 있는것
+
 // window size - dom 을 이용해서 html 내용 변겅, 윈도우 사이즈 값 출력하기
 
 const windowSize = document.querySelector(".window-background");
